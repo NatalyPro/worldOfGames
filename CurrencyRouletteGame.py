683e4201c753c6ed9dac434ab12c8e22de828098
Pro, Nata <natalyperkas@gmail.com>
2023-01-29 20:54:43 +0200

    update

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -3,6 +3,7 @@ import json
 from datetime import date, datetime, timedelta
 from MemoryGame import select_difficulty, check_value_is_correct, is_list_equal
 from GuessGame import generate_number
+import math
 
 
 def get_currency_exchange_rate(currency_from, currency_to, start_date, end_date):
@@ -17,7 +18,7 @@ def get_currency_exchange_rate(currency_from, currency_to, start_date, end_date)
         print(r["average_bid"])
         value = r["average_bid"]
 
-        return value
+        return float(value)
 
     # new_response = [{"currency": repo["average_bid"]} for repo in response_dict.json()]
     # print(new_response)
@@ -40,14 +41,13 @@ def get_yesterday_date():
 
 
 def play():
-    selected_difficulty = check_value_is_correct(select_difficulty(1, 6), 6, 1, 'difficulty')
+    selected_difficulty = check_value_is_correct(select_difficulty(1, 5), 5, 1, 'difficulty')
     show_generated_number_to_player = generate_number(101)
     number_from_player = check_value_is_correct(get_guess_number_from_player(show_generated_number_to_player), 99999999,
                                                 1, 'exchange rate')
     current_exchange_rate = get_currency_exchange_rate('USD', 'ILS', get_yesterday_date(), date.today())
-    get_money_interval(current_exchange_rate, selected_difficulty)
-    # is_list_equal replace with math.isclose()
-    if is_list_equal(number_from_player, get_money_interval(current_exchange_rate, selected_difficulty)):
+    if get_money_interval(calculate_exchange_rate_with_amount(current_exchange_rate, show_generated_number_to_player),
+                          selected_difficulty, number_from_player):
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! YOU WON !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     else:
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! YOU LOST !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
@@ -58,8 +58,31 @@ def get_guess_number_from_player(generated_number):
     return generated_number_from_user
 
 
-def get_money_interval(currency_rate, difficulty):
+def get_money_interval_not_use(currency_rate, difficulty):
     currency_rate_float = float(currency_rate)
     money_interval = (currency_rate_float - (5 - difficulty), currency_rate_float + (5 - difficulty))
     print(money_interval)
     return money_interval
+
+
+def calculate_exchange_rate_with_amount(exchange_rate, random_number):
+    calculated_amount = exchange_rate * random_number
+    return calculated_amount
+
+
+def get_money_interval(current_exchange_rate, difficulty, received_number_from_player):
+    if difficulty == 1:
+        if math.isclose(current_exchange_rate, received_number_from_player, abs_tol=20):
+            return True
+    if difficulty == 2:
+        if math.isclose(current_exchange_rate, received_number_from_player, abs_tol=15):
+            return True
+    if difficulty == 3:
+        if math.isclose(current_exchange_rate, received_number_from_player, abs_tol=10):
+            return True
+    if difficulty == 4:
+        if math.isclose(current_exchange_rate, received_number_from_player, abs_tol=5):
+            return True
+    if difficulty == 5:
+        if math.isclose(current_exchange_rate, received_number_from_player, abs_tol=1):
+            return True
